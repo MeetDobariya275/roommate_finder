c768a98be1d6a4ed333f900120bd4dd436cb7ba8
MeetDobariya275 <128107728+MeetDobariya275@users.noreply.github.com>
2024-09-12 16:09:07 -0700

    debug = false

--- roommate_finder/settings.py
+++ roommate_finder/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-<your-secret-key-here>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Define allowed hosts
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
