d7044087fa2e2ce2dca4d82b3d89999b8d30aa7e
MeetDobariya275 <128107728+MeetDobariya275@users.noreply.github.com>
2024-09-13 13:11:43 -0700

    path updated

--- roommate_finder/settings.py
+++ roommate_finder/settings.py
@@ -112,11 +112,9 @@ USE_TZ = True
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = 'home'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static'),]
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
