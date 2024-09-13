04cb795579c6c1969a5eaa1a08c3a360263b93a5
MeetDobariya275 <128107728+MeetDobariya275@users.noreply.github.com>
2024-09-13 12:06:52 -0700

    .

--- roommate_finder/settings.py
+++ roommate_finder/settings.py
@@ -27,6 +27,8 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 # Define allowed hosts
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Application definition
 
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
